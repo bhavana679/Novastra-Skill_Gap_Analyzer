8713721dce913916f72ce2f12ec721adb3d7b166
bhavana679 <bhavanatalkute@gmail.com>
2026-01-26 16:59:40 +0530

    added static learning resource library

---
+++ backend/src/config/resourceLibrary.js
@@ -0,0 +1,106 @@
+/**
+ * Static library of learning resources for various skills.
+ * This acts as a predefined database of helpful links for users.
+ */
+const RESOURCE_LIBRARY = {
+    "HTML": [
+        {
+            type: "docs",
+            title: "MDN Web Docs - HTML Basics",
+            url: "https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics",
+            level: "Beginner"
+        },
+        {
+            type: "video",
+            title: "HTML Full Course - Build a Website",
+            url: "https://www.youtube.com/watch?v=pQN-pnXPaVg",
+            level: "Beginner"
+        }
+    ],
+    "CSS": [
+        {
+            type: "docs",
+            title: "CSS-Tricks - A Complete Guide to Flexbox",
+            url: "https://css-tricks.com/snippets/css/a-guide-to-flexbox/",
+            level: "Intermediate"
+        },
+        {
+            type: "video",
+            title: "CSS Crash Course For Absolute Beginners",
+            url: "https://www.youtube.com/watch?v=yfoY53QXEnI",
+            level: "Beginner"
+        }
+    ],
+    "JavaScript": [
+        {
+            type: "docs",
+            title: "JavaScript.info - The Modern JavaScript Tutorial",
+            url: "https://javascript.info/",
+            level: "Beginner"
+        },
+        {
+            type: "course",
+            title: "Eloquent JavaScript (Free eBook)",
+            url: "https://eloquentjavascript.net/",
+            level: "Intermediate"
+        }
+    ],
+    "React": [
+        {
+            type: "docs",
+            title: "Official React Documentation",
+            url: "https://react.dev/learn",
+            level: "Beginner"
+        },
+        {
+            type: "video",
+            title: "React JS Crash Course 2024",
+            url: "https://www.youtube.com/watch?v=CgkZ7MvWUAA",
+            level: "Beginner"
+        }
+    ],
+    "Node.js": [
+        {
+            type: "docs",
+            title: "Node.js Guide for Beginners",
+            url: "https://nodejs.dev/en/learn/",
+            level: "Beginner"
+        },
+        {
+            type: "video",
+            title: "Node.js Crash Course",
+            url: "https://www.youtube.com/watch?v=fBNz5xF-Kx4",
+            level: "Intermediate"
+        }
+    ],
+    "MongoDB": [
+        {
+            type: "video",
+            title: "MongoDB Crash Course",
+            url: "https://www.youtube.com/watch?v=ofme2o29ngU",
+            level: "Beginner"
+        },
+        {
+            type: "docs",
+            title: "MongoDB University - Free Courses",
+            url: "https://university.mongodb.com/",
+            level: "Intermediate"
+        }
+    ],
+    "TypeScript": [
+        {
+            type: "docs",
+            title: "TypeScript Handbook",
+            url: "https://www.typescriptlang.org/docs/handbook/intro.html",
+            level: "Intermediate"
+        },
+        {
+            type: "video",
+            title: "TypeScript Tutorial for Beginners",
+            url: "https://www.youtube.com/watch?v=gieEQFIfgTk",
+            level: "Beginner"
+        }
+    ]
+};
+
+export default RESOURCE_LIBRARY;
